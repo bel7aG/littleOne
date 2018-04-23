b961029bb90823a7e3f52bad01f4da1df0f521d3
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-04-23 23:44:11 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+'use strict';
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header/index';
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h1>Welcome To React</h1>
         <h2>Wassup React im comming to talk with for some change</h2>
+        <h1>okok</h1>
       </div>
     );
   }
