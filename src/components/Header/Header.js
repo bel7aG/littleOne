8202de58d2204d07b71287dcc58faf86824d1b00
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-04-23 23:43:16 +0200

    extend Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+'use strict';
+import React, { Component } from 'react';
+import "./Header.css";
+const Header = () => (
+  <div>
+    <h1>Welcome To React World from the big Door</h1>
+  </div>
+);
+
+export default Header;
